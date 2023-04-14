c2b9f6f05b34aa20c28f19924db232bdd1b86479
Edwin Lee <leeed2001@gmail.com>
2023-04-13 21:27:26 -0500

    Polish setup.py, bump version

--- setup.py
+++ setup.py
@@ -17,16 +17,35 @@ setup(
     version=VERSION,
     description='A library and tkinter-based tool for transitioning EnergyPlus input files',
     url='https://github.com/myoldmopar/EnergyPlusTransitionTools',
-    license='',
+    license='ModifiedBSD',
     packages=['energyplus_transition'],
     package_data={"energyplus_transition": ["icons/*.png", "icons/*.ico", "icons/*.icns"],},
     include_package_data=True,
     long_description=readme_contents,
     long_description_content_type='text/markdown',
-    author="Edwin Lee via NREL via United States Department of Energy",
+    author="Edwin Lee, for NREL, for the United States Department of Energy",
     install_requires=install_requires,
     entry_points={
         'gui_scripts': ['energyplus_transition_gui=energyplus_transition.runner:main_gui'],
         'console_scripts': ['energyplus_transition_configure=energyplus_transition.configure:configure_cli']
-    }
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Utilities',
+    ],
+    platforms=[
+        'Linux (Tested on Ubuntu)', 'MacOSX', 'Windows'
+    ],
+    keywords=[
+        'energyplus_launch', 'ep_launch',
+        'EnergyPlus', 'eplus', 'Energy+',
+        'Building Simulation', 'Whole Building Energy Simulation',
+        'Heat Transfer', 'HVAC', 'Modeling',
+    ]
 )
