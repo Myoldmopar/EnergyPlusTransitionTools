2653e099e348c32921fe23b5b22cd684157d464a
Julien Marrec <julien.marrec@gmail.com>
2026-06-02 11:00:19 +0200

    100% coverage for international.py

--- tests/test_international.py
+++ tests/test_international.py
@@ -1,18 +1,32 @@
 from unittest import TestCase
 
-from energyplus_transition.international import report_missing_keys, set_language, translate, Language
+from energyplus_transition.international import Language, report_missing_keys, set_language, translate
 
 
 class TestInternational(TestCase):
     def test_missing_keys(self) -> None:
         self.assertFalse(report_missing_keys(mute=True))  # add assertion here
 
-    def test_translation(self) -> None:
+    def test_translaiton_english(self) -> None:
+        set_language(lang=Language.English)
+        self.assertEqual("", translate(key=None))  # type: ignore[arg-type]
+        self.assertEqual("", translate(key=""))
+        self.assertEqual("TRANSLATION MISSING", translate(key="WHAT", mute=True))
+        self.assertEqual("TRANSLATION MISSING", translate(key="WHAT", mute=True))
+        self.assertEqual("Close", translate(key="Close", mute=True))
+
+    def test_translation_spanish(self) -> None:
         set_language(lang=Language.Spanish)
         self.assertEqual("", translate(key=None))  # type: ignore[arg-type]
         self.assertEqual("", translate(key=""))
         self.assertEqual("TRANSLATION MISSING", translate(key="WHAT", mute=True))
         self.assertEqual("TRANSLATION MISSING", translate(key="WHAT", mute=True))
         self.assertEqual("Cerca", translate(key="Close", mute=True))
+
+    def test_translation_french(self) -> None:
         set_language(lang=Language.French)
+        self.assertEqual("", translate(key=None))  # type: ignore[arg-type]
+        self.assertEqual("", translate(key=""))
+        self.assertEqual("TRANSLATION MISSING", translate(key="WHAT", mute=True))
+        self.assertEqual("TRANSLATION MISSING", translate(key="WHAT", mute=True))
         self.assertEqual("Fermer", translate(key="Close", mute=True))
